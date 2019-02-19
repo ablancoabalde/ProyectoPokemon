f3d48c0013419ae2c1879c38939d8d1745b02b56
ablancoabalde <alberto.lya@gmail.com>
2019-02-19 21:49:58 +0100

    Initial commit

---
+++ app/src/main/java/com/alberto/intercambio_pokemon/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.alberto.intercambio_pokemon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
