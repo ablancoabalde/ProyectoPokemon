a8e3f5f8443b44b758c2c8a072c7f6ac26d61640
ablancoabalde <alberto.lya@gmail.com>
2019-02-27 10:52:48 +0100

    Commit FireBaseMessaginn
    Introducimos la forma de recibir notificaciones desde nuestro node.js

--- app/src/main/java/com/alberto/intercambio_pokemon/MainActivity.kt
+++ app/src/main/java/com/alberto/intercambio_pokemon/MainActivity.kt
@@ -3,48 +3,10 @@ package com.alberto.intercambio_pokemon
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView;
-// Para importar la librería de la database
-import com.google.firebase.database.*
-// Importar las funciones de los LOG
-import android.util.Log
-import android.view.View
-import android.widget.Button
-// Importar la función de llamar a los elementos del xml
+
 import kotlinx.android.synthetic.main.activity_main.*
 
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseReference
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import android.support.design.widget.TextInputLayout
-
-
-
-
-
-
-
-
 class MainActivity : AppCompatActivity() {
-    // para filtrar los logs
-
-
-    // // referencia a la base de datos del proyecto en firebase
-  //  private var dbCielo: DatabaseReference? = FirebaseDatabase.getInstance().getReference()
-
-   // private var eventListener: ValueEventListener? = null
-
-
-
-   // private val lstPredicciones: RecyclerView? = null
-
-   // var mAdapter: FirebaseRecyclerAdapter<*, *>? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,71 +25,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-
-
-        // Primer punto
-        /*
-         dbCielo = FirebaseDatabase.getInstance().getReference("predicciones");
-
-
-
-        eventListener = (object : ValueEventListener {
-
-            /**
-             * Función que llamará automáticamente cada vez que se actualice
-             * la información del nodo actual o se produzca cualquier cambio en cualquiera de sus nodos descendientes
-             */
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-               // val valor = dataSnapshot.value
-               // lblCielo.text = valor.toString()
-
-                lblUsuario.setText(dataSnapshot.key.toString());
-                lblCielo.setText(dataSnapshot.child("cielo").getValue().toString());
-                lblTemperatura.setText(dataSnapshot.child("temperatura").getValue().toString());
-                lblHumedad.setText(dataSnapshot.child("humedad").getValue().toString());
-            }
-
-            /**
-             * Función que llamará cuando la lectura de los datos sea cancelada por cualquier motivo,
-             * por ejemplo porque el usuario no tiene permiso para acceder a los datos.
-             */
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.e(TAG, "Error!", databaseError.toException())
-            }
-        })
-        dbCielo!!.addValueEventListener(eventListener as ValueEventListener);
-
-        // Otro ejemplo con días de las semana
-        val dbDiasSemana = FirebaseDatabase.getInstance().reference
-            .child("dias-semana")
-
-        val childEventListener = object : ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                Log.d(TAG, "onChildAdded: {" + dataSnapshot.key + ": " + dataSnapshot.value + "}")
-            }
-
-            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                Log.d(TAG, "onChildChanged: {" + dataSnapshot.key + ": " + dataSnapshot.value + "}")
-            }
-
-            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
-                Log.d(TAG, "onChildRemoved: {" + dataSnapshot.key + ": " + dataSnapshot.value + "}")
-            }
-
-            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                Log.d(TAG, "onChildMoved: " + dataSnapshot.key!!)
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.e(TAG, "Error!", databaseError.toException())
-            }
-        }
-
-        dbDiasSemana.addChildEventListener(childEventListener)
-        */
-
-
 
     }
 
--- app/src/main/java/com/alberto/intercambio_pokemon/MyFireBaseMessagingService.kt
+++ app/src/main/java/com/alberto/intercambio_pokemon/MyFireBaseMessagingService.kt
@@ -1,8 +1,59 @@
 package com.alberto.intercambio_pokemon
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFireBaseMessagingService: FirebaseMessagingService() {
+    val TAG = "FirebaseMessagingService"
 
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "De donde llega: ${remoteMessage.from}")
+        Log.d(TAG, "TEXTO: ${remoteMessage.notification}")
+
+
+        val texto = remoteMessage.notification?.body
+
+
+
+        if (remoteMessage.notification != null) {
+            val titulo = remoteMessage.notification?.title
+            val texto = remoteMessage.notification?.body
+
+            Log.d(TAG, "NOTIFICACION RECIBIDA")
+            Log.d(TAG, "Título: $titulo")
+            Log.d(TAG, "Texto: $texto")
+
+            showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+        }
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 
 }
