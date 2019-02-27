cf041dcd8bbbc7744a411e8de129c9555116665b
ablancoabalde <alberto.lya@gmail.com>
2019-02-27 10:46:34 +0100

    Commit Interfaz
    Posicionando el cófigo de este

--- app/src/main/java/com/alberto/intercambio_pokemon/Interfaz.kt
+++ app/src/main/java/com/alberto/intercambio_pokemon/Interfaz.kt
@@ -4,8 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_interfaz.*
 import java.util.*
 
@@ -17,83 +16,170 @@ class Interfaz : AppCompatActivity() {
     // // referencia a la base de datos del proyecto en firebase
     private var fireDatabase= FirebaseDatabase.getInstance().getReference()
 
-    var Inombre: String = ""
-    var INomPokemon: String = ""
-    var ICantidad: String = ""
+    var iNnombre: String = ""
+    var iNomPokemon: String = ""
+    var iCantidad: String = ""
 
-    var IarrayList: MutableList<Pokemons> = mutableListOf()
+    var iArrayList: MutableList<Pokemons> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interfaz)
 
-        Inombre= Variables.cajaUsuario
-
-
-
+        iNnombre= Variables.cajaUsuario
 
         btn_ver.setOnClickListener{
             val lPokemon = Intent(this, ListasPokemon::class.java)
             startActivity(lPokemon)
         }
 
-        btn_mas.setOnClickListener {
-            INomPokemon = txtPokemon.text.toString().trim()
-            ICantidad = txtCantidad.text.toString().trim()
-            if(INomPokemon.isEmpty()||ICantidad.isEmpty()){
-                validation(INomPokemon,ICantidad)
-            }else{
-                val pk = Pokemons()
-                pk.nombrePokemon=INomPokemon
-                pk.cantidad=ICantidad
-                IarrayList.add(pk)
-                limpiarCajas()
-            }
-        }
+        initListener()
+    }
 
-        btn_enviar.setOnClickListener {
-            INomPokemon = txtPokemon.text.toString().trim()
-            ICantidad = txtCantidad.text.toString().trim()
+    private fun initListener() {
+        val childEventListener = object : ChildEventListener {
 
-            if(IarrayList.isEmpty()){
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
 
-                if(INomPokemon.isEmpty()||ICantidad.isEmpty()){
-                    validation(INomPokemon,ICantidad)
-                }else{
+                for (snapshot in dataSnapshot.child(iNnombre).child("arrayList").children) {
+                    iArrayList.clear()
+                    var p:Pokemons= snapshot.getValue(Pokemons::class.java)!!
                     val pk = Pokemons()
-                    pk.nombrePokemon=INomPokemon
-                    pk.cantidad=ICantidad
-                    IarrayList.add(pk)
-
-                    val p = Persona()
-                    p.id=UUID.randomUUID().toString()
-                    p.nombre=Inombre
-                    p.arrayList=IarrayList
-                    fireDatabase.child("Usuario").child(p.id).setValue(p)
-                    limpiarCajas()
+                    pk.nombrePokemon=p.nombrePokemon
+                    pk.cantidad=p.cantidad
+                    iArrayList.add(p)
                 }
-            }else{
-                val p = Persona()
-                p.id=UUID.randomUUID().toString()
-                p.nombre=Inombre
-                p.arrayList=IarrayList
-                fireDatabase.child("Usuario").child(p.id).setValue(p)
-                limpiarCajas()
+
+                btn_mas.setOnClickListener {
+                    iNomPokemon = txtPokemon.text.toString().trim()
+                    iCantidad = txtCantidad.text.toString().trim()
+                    if(iNomPokemon.isEmpty()||iCantidad.isEmpty()){
+                        validation(iNomPokemon,iCantidad)
+                    }else{
+                        val pk = Pokemons()
+                        pk.nombrePokemon=iNomPokemon
+                        pk.cantidad=iCantidad
+                        iArrayList.add(pk)
+                        limpiarCajas()
+                    }
+                }
+
+                btn_enviar.setOnClickListener {
+                    iNomPokemon = txtPokemon.text.toString().trim()
+                    iCantidad = txtCantidad.text.toString().trim()
+
+                    if(iArrayList.isEmpty()){
+
+                        if(iNomPokemon.isEmpty()||iCantidad.isEmpty()){
+                            validation(iNomPokemon,iCantidad)
+                        }else{
+                            val pk = Pokemons()
+                            pk.nombrePokemon=iNomPokemon
+                            pk.cantidad=iCantidad
+                            iArrayList.add(pk)
+
+                            val p = Persona()
+                            p.nombre=iNnombre
+                            p.arrayList=iArrayList
+                            fireDatabase.child("Usuario").child(p.nombre).setValue(p)
+                            limpiarCajas()
+                        }
+                    }else{
+                        val p = Persona()
+                        p.nombre=iNnombre
+                        p.arrayList=iArrayList
+                        val pk = Pokemons()
+                        pk.nombrePokemon=iNomPokemon
+                        pk.cantidad=iCantidad
+                        iArrayList.add(pk)
+                        fireDatabase.child("Usuario").child(p.nombre).setValue(p)
+                        limpiarCajas()
+                    }
+
+                }
             }
 
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                iArrayList.clear()
+                for (snapshot in dataSnapshot.child(iNnombre).child("arrayList").children) {
 
+                    var p:Pokemons= snapshot.getValue(Pokemons::class.java)!!
+                    val pk = Pokemons()
+                    pk.nombrePokemon=p.nombrePokemon
+                    pk.cantidad=p.cantidad
+                    iArrayList.add(p)
+                }
 
+                btn_mas.setOnClickListener {
+                    iNomPokemon = txtPokemon.text.toString().trim()
+                    iCantidad = txtCantidad.text.toString().trim()
+                    if(iNomPokemon.isEmpty()||iCantidad.isEmpty()){
+                        validation(iNomPokemon,iCantidad)
+                    }else{
+                        val pk = Pokemons()
+                        pk.nombrePokemon=iNomPokemon
+                        pk.cantidad=iCantidad
+                        iArrayList.add(pk)
+                        limpiarCajas()
+                    }
+                }
 
+                btn_enviar.setOnClickListener {
+                    iNomPokemon = txtPokemon.text.toString().trim()
+                    iCantidad = txtCantidad.text.toString().trim()
+
+                    if(iArrayList.isEmpty()){
+
+                        if(iNomPokemon.isEmpty()||iCantidad.isEmpty()){
+                            validation(iNomPokemon,iCantidad)
+                        }else{
+                            val pk = Pokemons()
+                            pk.nombrePokemon=iNomPokemon
+                            pk.cantidad=iCantidad
+                            iArrayList.add(pk)
+
+                            val p = Persona()
+                            p.nombre=iNnombre
+                            p.arrayList=iArrayList
+                            fireDatabase.child("Usuario").child(p.nombre).setValue(p)
+                            limpiarCajas()
+                        }
+                    }else{
+                        val p = Persona()
+                        p.nombre=iNnombre
+                        p.arrayList=iArrayList
+                        fireDatabase.child("Usuario").child(p.nombre).setValue(p)
+                        limpiarCajas()
+                    }
+
+                }
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved: {" + dataSnapshot.key + ": " + dataSnapshot.value + "}")
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved: " + dataSnapshot.key!!)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e(TAG, "Error!", databaseError.toException())
+            }
         }
 
+        fireDatabase.addChildEventListener(childEventListener)
 
     }
 
 
-
     private fun limpiarCajas(){
-        txtPokemon.text?.clear()
-        txtCantidad.text?.clear()
+        txtPokemon.text!!.clear()
+        Log.e(TAG, "Limpiar cajas !${txtPokemon.text}")
+        txtCantidad.text!!.clear()
+        Log.e(TAG, "Limpiar cajas !${txtCantidad.text}")
+        iNomPokemon= ""
+        iCantidad = ""
     }
 
     private fun validation(Nombre : String, Cantidad : String ) {
